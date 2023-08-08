fb157894efc04155bcd53bf477c0072cf4dac71f
Anjutechversant22 <112407649+Anjutechversant22@users.noreply.github.com>
2023-08-08 16:37:35 +0530

    first commit

---
+++ celerytaskone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for celerytaskone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celerytaskone.settings')
+
+application = get_asgi_application()
